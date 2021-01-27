7e5f8ea99b323920ed8fe6e938e2b04982587e88
zippochoiloto <theanhptit94@gmail.com>
2021-01-28 06:52:25 +0700

    add min heap class

--- test.js
+++ test.js
@@ -192,4 +192,30 @@ var search = function(nums, target) {
     return -1;
 };
 
-console.log(search([1,2,3,2],3))
+// console.log(search([1,2,3,2],3))
+
+let nums1 = [1,1,1,1,1]
+let s = 3
+
+var findTargetSumWays = function(nums, S) {
+    let sums = new Map([[0, 1]]);
+    
+    for (let num of nums) {
+        const next = new Map();
+        
+        for (let [sum, amount] of sums) {
+            const plus = sum + num;
+            const minus = sum - num;
+        
+            next.set(plus, next.has(plus) ? next.get(plus) + amount : amount);
+            next.set(minus, next.has(minus) ? next.get(minus) + amount : amount);
+        }
+        
+        sums = next;
+    }
+    
+    return sums.has(S) ? sums.get(S) : 0;
+    };
+
+
+console.log(findTargetSumWays(nums1, s))
